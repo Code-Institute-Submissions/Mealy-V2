ea830ed628b07d7f59e0d2a3cb9c6c3395f53064
Cormac Hogan <Cormac_Hogan@protonmail.com>
2022-08-30 19:51:16 +0000

    remove planned meal attribute

--- meals/models.py
+++ meals/models.py
@@ -1,23 +1,25 @@
 from django.db import models
-from django.contrib.auth.models import User         
+from django.contrib.auth.models import User
+
 # Create your models here.
+
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=200)
+
     def __str__(self):
         return self.name
 
 
 class Meal(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, blank=True
+    )
     meal_name = models.CharField(max_length=200)
-    planned = models.BooleanField(default=False)
-    ingredients = models.ManyToManyField(Ingredient,blank=True)
-    meal_description = models.TextField(max_length=1000, default="Yummy meal description goes here")
-    
-    
+    ingredients = models.ManyToManyField(Ingredient, blank=True)
+    meal_description = models.TextField(
+        max_length=1000, default="Yummy meal description goes here"
+    )
+
     def __str__(self):
         return self.meal_name
-
-    class Meta:
-        ordering = ["-planned"]
--- meals/urls.py
+++ meals/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 urlpatterns = [
     path("", views.MealList.as_view(), name="home"),
     path("list", views.ShopList.as_view(), name="shopping_list"),
-    path("toggle_planned/<meal_id>", views.toggle_planned, name="toggle"),
     path("delete_meal/<meal_id>", views.delete_meal, name="deleted"),
-    path("add", views.add_ingredient, name="add"),]
+    path("add", views.add_ingredient, name="add"),
+]
