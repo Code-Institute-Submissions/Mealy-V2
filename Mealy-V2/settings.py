a416241ecf97b8ecfe40c77ccdf95fa7b4d52a97
Cormac Hogan <Cormac_Hogan@protonmail.com>
2022-08-31 02:52:07 +0000

    remove debug

--- Mealy-V2/settings.py
+++ Mealy-V2/settings.py
@@ -32,10 +32,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-DEBUG = True
 ALLOWED_HOSTS = ["mealyapp-v2.herokuapp.com"]
 
-
+DEBUG = development
 # Application definition
 
 INSTALLED_APPS = [
