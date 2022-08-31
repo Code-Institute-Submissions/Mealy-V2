4d7d8aa91614021172461524cd1f791889e76dcf
Cormac Hogan <Cormac_Hogan@protonmail.com>
2022-08-31 02:40:40 +0000

    fix host

--- Mealy-V2/settings.py
+++ Mealy-V2/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 DEBUG = True
-ALLOWED_HOSTS = ["127.0.0.1", "mealyapp-v2.herokuapp.com"]
+ALLOWED_HOSTS = ["mealyapp-v2.herokuapp.com"]
 
 
 # Application definition
